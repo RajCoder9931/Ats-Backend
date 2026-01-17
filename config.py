5affdb56cad2b0eee5b8d561a1ac133a26118fbc
Ravi Raj <tiwariraviraj50@gmail.com>
2026-01-17 11:23:59 +0530

    done

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 MONGO_URI = "mongodb+srv://Jobboard:Argosmob12@cluster0.0xuryoi.mongodb.net/RecuritApp"
 JWT_SECRET = "SUPER_SECRET_KEY_CHANGE_THIS"
-ACCESS_TOKEN_MINUTES = 15
+ACCESS_TOKEN_MINUTES = 20
 REFRESH_TOKEN_DAYS = 7
